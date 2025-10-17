979c882c3adca289ee7d0fe6f29f0b9bdb96015a
frol1k3 <alex_frolov24042007@mail.ru>
2025-10-17 20:28:33 +0300

    созал модель доктора

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100, verbose_name="ФИО")
+    speciality = models.TextField(verbose_name="Специальность")
+    work_experience = models.IntegerField(verbose_name="Стаж работы")
