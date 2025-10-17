c86ec3a2bcf6d90f37c169e33a062039e9243853
frol1k3 <alex_frolov24042007@mail.ru>
2025-10-17 20:46:42 +0300

    создал базовый шаблон html

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class Doctor(models.Model):
     name = models.CharField(max_length=100, verbose_name="ФИО")
     speciality = models.TextField(verbose_name="Специальность")
     work_experience = models.IntegerField(verbose_name="Стаж работы")
+
