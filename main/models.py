e711c8d20a1f924703c62410e8cfffc2cd71cf36
frol1k3 <alex_frolov24042007@mail.ru>
2025-10-29 21:10:06 +0300

    Добавил подель пациента

--- main/models.py
+++ main/models.py
@@ -6,3 +6,22 @@ class Doctor(models.Model):
     speciality = models.TextField(verbose_name="Специальность")
     work_experience = models.IntegerField(verbose_name="Стаж работы")
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Доктор"
+        verbose_name_plural = "Доктора"
+
+class Patient(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Имя")
+    surename = models.CharField(max_length=20, verbose_name="Фамилия")
+    login = models.TextField(verbose_name="Логин")
+    password = models.TextField(verbose_name="Пароль")
+
+    def __str__(self):
+        return self.login
+    
+    class Meta:
+        verbose_name = "Пациент"
+        verbose_name_plural = "Пациенты"
