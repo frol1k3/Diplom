639cab484fc1b88592f2f9459427e819a9e0a4c3
frol1k3 <alex_frolov24042007@mail.ru>
2025-10-31 21:02:23 +0300

    добавил модель записей и поле рабочих дней у докторов

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Doctor(models.Model):
     name = models.CharField(max_length=100, verbose_name="ФИО")
     speciality = models.TextField(verbose_name="Специальность")
     work_experience = models.IntegerField(verbose_name="Стаж работы")
+    work_day = models.CharField(verbose_name="Рабочие дни недели", default="пн, ср, пт")
 
     def __str__(self):
         return self.name
@@ -25,3 +26,20 @@ class Patient(models.Model):
     class Meta:
         verbose_name = "Пациент"
         verbose_name_plural = "Пациенты"
+
+class Record(models.Model):
+    time = models.DateTimeField(verbose_name="Время записи")
+    patient = models.ForeignKey(Patient, on_delete=models.PROTECT)
+    doctor = models.ForeignKey(Doctor, on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.time
+    
+    class Meta:
+        verbose_name = "Запись"
+        verbose_name_plural = "Записи"
+
+
+
+   
